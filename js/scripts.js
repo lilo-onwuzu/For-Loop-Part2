43f210b90d452d859fecc07def2ab379d813892e
lilo-onwuzu <lpr422@gmail.com>
2017-05-01 17:50:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-// For Loop Multiplying Arbitrary # of #s
-// var blanks = [1];
-  $("form#count").submit(function(event){
-    event.preventDefault();
-
     // var icecreams = []
     // var ice1Name = $("input#icecream1").val();
     // var ice2Name = $("input#icecream2").val();
@@ -14,7 +8,7 @@ $(document).ready(function(){
 
 // For Each Loop Icecream Implementation
     // icecreams.forEach(function(icecream){
-    // alert(icecream);
+    //  alert(icecream);
     // });
 
 // For Loop Icecream Implementation
@@ -22,21 +16,10 @@ $(document).ready(function(){
     //   $("#flavor" + index).text(icecreams[index]);
     // };
 
-// For Loop Multiplying Arbitrary # of #s
-    // var intNumber = parseInt($("input#number").val());
-    //
-    // for (var index = 0; index < blanks.length; index += 1){
-    //   var total = intNumber * blanks[index];
-    //   blanks.pop();
-    //   blanks.push(total);
-    //   console.log(total);
-    // };
-
-// WORD PUZZLE
-// Pushing one array into another as in var blank = [1];blank.push(strSplit); will push the "length" of the strSplit as blank[0]. The modified array blank  will be [1,length of strSplit]. But the other array (strSplit) stays the same as an array.
-// Pushing elements of an array one by one will modify the other array into an array with the elements of the new array included.
-// || or logic "OR" demarcates the left side from the right side.
-
+$(document).ready(function(){
+  $("form#count").submit(function(event){
+    event.preventDefault();
+    
 // User Interface Logic
       var enterWord = $("input#word").val();
       var strSplit = enterWord.split("");
@@ -54,55 +37,3 @@ $(document).ready(function(){
       console.log(outputString);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Word Puzzle
-
-// var str = $("input#word").val();
-// var vowels = []
-//
-// vowels.forEach(function(vowel)) {
-//   $('str').replace("a","-");
-// }
